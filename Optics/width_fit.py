abd5b73511acd4d60400acf6f7479dd6e28b5755
MT Computer <cama5239@colorado.edu>
2025-03-29 09:06:30 -0600

    Added a new file for the beam width analysis

---
+++ Optics/width_fit.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Short Distance Low Power
+
+
+
+
+
+
+## Short Distance High Power
+
+
+
+
+
+
+
+## Long Distance Low Power
+
+
+
+
+
+
+## Long Distance High Power
